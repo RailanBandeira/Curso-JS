dc589d478c19b50fa7e4058e20ad97bf78fb9d0d
Railan Andrade Bandeira <162853790+RailanBandeira@users.noreply.github.com>
2024-03-13 17:03:11 -0300

    Add files via upload

---
+++ JS/ex005/aula 11.js
@@ -0,0 +1,6 @@
+var velocidade = 60
+console.log(`A velocidade do carro é ${velocidade} Km/h`)
+if(velocidade > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Digija sempre com cinto de segurança!')
